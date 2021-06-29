3f6c7c03e04983f6b700108dde05f2f0de405b68
sunaoka <sunaoka@pocari.org>
2021-06-29 14:39:51 +0900

    Add comment

--- lib/rot13_converter.dart
+++ lib/rot13_converter.dart
@@ -2,8 +2,10 @@ library rot13_converter;
 
 import 'dart:convert';
 
+/// An instance of the default implementation of the [Rot13Converter].
 const ROT13 = const Rot13Converter();
 
+/// A Lookup table
 const Map<String, String> _table = {
   'A': 'N',
   'B': 'O',
@@ -59,9 +61,11 @@ const Map<String, String> _table = {
   'z': 'm',
 };
 
+/// A converter to encode and decode plain string into ROT13 string.
 class Rot13Converter extends Converter<String, String> {
   const Rot13Converter();
 
+  /// convert [data] into ROT13 string
   String convert(String data) {
     final List<String> encrypted = [];
     for (int i = 0, length = data.length; i < length; ++i) {
